591fbbc891193bb3b0c52a06ec06273a928c3ac4
ltcoder740 <ltcoder740@gmail.com>
2020-03-16 17:03:15 +0530

    Add app js file

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+import 'typeface-roboto';
+import store from './app/store';
+
+const render = () => {
+  const App = require('./app/App').default
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render()
+serviceWorker.unregister();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app/App', render)
+}
