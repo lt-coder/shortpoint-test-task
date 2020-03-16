49a8821a22f48959c72ffc0d85d141146943cadf
ltcoder740 <ltcoder740@gmail.com>
2020-03-16 17:05:13 +0530

    Add custom paper component

---
+++ src/components/CustomPaper/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+  },
+}));
+
+const CustomPaper = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.paper}>
+      {children}
+    </Paper>
+  );
+}
+
+CustomPaper.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
+
+export default CustomPaper;
